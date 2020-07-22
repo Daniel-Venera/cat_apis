1371242c5f3e72b55facdc7f2f32a1d8330eda67
Daniel-Venera <daniel.venera.programmer@gmail.com>
2020-07-22 10:28:36 +0200

    cat fact api added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
+let factButton = document.querySelector(".btn-fact");
+let factText = document.querySelector(".fact-text");
+
 let imageButton = document.querySelector(".btn-image");
 let imageContainer = document.querySelector(".image-container");
 let catImage = document.querySelector(".cat-image");
+
 imageButton.addEventListener("click", getCatImage);
+factButton.addEventListener("click", getCatFact);
 
 function getCatImage() {
   fetch("https://api.thecatapi.com/v1/images/search")
@@ -15,4 +20,17 @@ function displayImage(response) {
   catImage.src = response[0].url;
 }
 
+function getCatFact() {
+  fetch("https://cat-fact.herokuapp.com/facts")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(displayFact);
+}
+
+function displayFact(response) {
+  factText.innerText = response.all[Math.floor(Math.random() * 238)].text;
+}
+
 getCatImage();
+getCatFact();
